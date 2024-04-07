a6185ac5893f35ec0ac3d7eca2a3498e2365e30f
Sebastián Ayala <se.ayalas@duocuc.cl>
2024-04-06 22:55:15 -0300

    Refactor addToCart function in carrito.js to include item price

--- carrito.js
+++ carrito.js
@@ -1,13 +1,13 @@
 var cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-function addToCart(item) {
-    cart.push(item);
+function addToCart(item, price) {
+    cart.push({name: item, price: price});
     localStorage.setItem('cart', JSON.stringify(cart));
     displayCart();
+    alert(item + ' fue añadido a tu carrito!');
 }
-
 function removeFromCart(item) {
-    var index = cart.indexOf(item);
+    var index = cart.findIndex(cartItem => cartItem.name === item);
     if (index > -1) {
         cart.splice(index, 1);
     }
@@ -20,6 +20,13 @@ function displayCart() {
     cartItemsDiv.innerHTML = '';
     var cart = JSON.parse(localStorage.getItem('cart')) || [];
     for (var i = 0; i < cart.length; i++) {
-        cartItemsDiv.innerHTML += '<div class="dropdown-item">' + cart[i] + '<button onclick="removeFromCart(\'' + cart[i] + '\')">Remove</button></div>';
+        cartItemsDiv.innerHTML += '<div class="dropdown-item">' + cart[i].name + ' - ' + cart[i].price + '<button onclick="removeFromCart(\'' + cart[i].name + '\')">Eliminar</button></div>';
     }
 }
+
+function clearCart() {
+    cart = [];
+    localStorage.setItem('cart', JSON.stringify(cart));
+    displayCart();
+}
+
